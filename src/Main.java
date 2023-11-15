1dd3572c658b096aa3415baec4b01ff72a02c5ba
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2023-11-15 14:13:42 +0100

    feat: remove test connection

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,5 @@ import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
-      test();
-    }
-    public static Connection test(){
-        ConnectionDB connectionDB = new ConnectionDB();
-        try (Connection connection = connectionDB.createConnection()) {
-            System.out.println("Connection to the database successful!");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 }
