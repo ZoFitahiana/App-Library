c31f59db19b48418a8685f5fe15613e4ae75dac4
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2023-11-15 19:34:52 +0100

    feat: create class Administrator

---
+++ src/Model/Administrator.java
@@ -0,0 +1,54 @@
+package Model;
+
+import java.util.Objects;
+
+public class Administrator extends User{
+    private String role;
+    private String userName;
+    private String password;
+
+    public Administrator(String id, String name, String firstName, String email, String genre, String role, String userName, String password) {
+        super(id, name, firstName, email, genre);
+        this.role = role;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Administrator that = (Administrator) o;
+        return Objects.equals(role, that.role) && Objects.equals(userName, that.userName) && Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), role, userName, password);
+    }
+}
