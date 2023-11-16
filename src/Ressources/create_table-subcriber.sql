78909ecbef586b609386213151df808568fc925a
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2023-11-16 12:54:03 +0100

    fix: update ressouces table subscribe

--- src/Ressources/create_table-subcriber.sql
+++ src/Ressources/create_table-subcriber.sql
@@ -1,17 +1,17 @@
 -- create table subscriber
 
 CREATE TABLE IF NOT EXISTS  Subscribe(
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     first_name VARCHAR(255),
     email VARCHAR(255),
-    genre VARCHAR(10),
+    genre VARCHAR(10) CHECK (genre IN ('M','F')),
     number VARCHAR(15)
 );
 
 -- insert to subcriber
 INSERT INTO Subscribe(id, name, first_name, email, genre, number) VALUES
-    (1, 'Doe', 'John', 'john.doe@example.com', 'Male', '123456789'),
-    (2, 'Smith', 'Jane', 'jane.smith@example.com', 'Female', '987654321'),
-    (3, 'Johnson', 'Bob', 'bob.johnson@example.com', 'Male', '555555555');
+    (1, 'Doe', 'John', 'john.doe@example.com', 'M', '123456789'),
+    (2, 'Smith', 'Jane', 'jane.smith@example.com', 'F', '987654321'),
+    (3, 'Johnson', 'Bob', 'bob.johnson@example.com', 'M', '555555555');
 
