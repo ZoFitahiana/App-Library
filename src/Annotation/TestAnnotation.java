a2e4c74c70f585366f72ec091387697e4f1936cf
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2023-11-16 13:31:57 +0100

    feat: create test for MyFirstAnnotation

---
+++ src/Annotation/TestAnnotation.java
@@ -0,0 +1,16 @@
+package Annotation;
+
+public class TestAnnotation {
+    @MyFirstAnnotation
+    private int accumulator;
+
+    public TestAnnotation() {
+        this.accumulator = accumulator;
+    }
+
+    public int getAccumulator() {
+        System.out.println("the value of your accumulator is  : ");
+        return accumulator;
+    }
+
+}
