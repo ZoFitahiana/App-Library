95c7189870ce74b9505df4cb598a0ac5a55d8765
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2023-11-16 08:48:20 +0100

    fix:update model Book

--- src/Model/Book.java
+++ src/Model/Book.java
@@ -4,15 +4,20 @@ import java.sql.Date;
 import java.util.Objects;
 
 public class Book {
+
+    public enum Topic {
+        COMEDY, ROMANCE, OTHER
+    }
+
     private int id;
     private String bookName;
     private int id_author;
     private int page_number;
-    private String topic;
+    private Topic topic;
     private Date release_date;
     private  String status;
 
-    public Book(int id, String bookName, int id_author, int page_number, String topic, Date release_date, String status) {
+    public Book(int id, String bookName, int id_author, int page_number, Topic topic, Date release_date, String status) {
         this.id = id;
         this.bookName = bookName;
         this.id_author = id_author;
@@ -26,56 +31,56 @@ public class Book {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getBookName() {
         return bookName;
+    }
+
+    public int getId_author() {
+        return id_author;
+    }
+
+    public int getPage_number() {
+        return page_number;
+    }
+
+    public Topic getTopic() {
+        return topic;
+    }
+
+    public Date getRelease_date() {
+        return release_date;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public void setBookName(String bookName) {
         this.bookName = bookName;
     }
 
-    public int getAuthor() {
-        return id_author;
-    }
-
-    public void setAuthor(String author) {
+    public void setId_author(int id_author) {
         this.id_author = id_author;
     }
 
-    public int getPageNumbres() {
-        return page_number;
+    public void setPage_number(int page_number) {
+        this.page_number = page_number;
     }
 
-    public void setPageNumbres(int pageNumbres) {
-        this.page_number = pageNumbres;
-    }
-
-    public String getTopic() {
-        return topic;
-    }
-
-    public void setTopic(String topic) {
+    public void setTopic(Topic topic) {
         this.topic = topic;
     }
 
-    public Date getReleaseDate() {
-        return release_date;
-    }
-
-    public void setReleaseDate(Date releaseDate) {
-        this.release_date = releaseDate;
-    }
-
-    public String getStatus() {
-        return status;
+    public void setRelease_date(Date release_date) {
+        this.release_date = release_date;
     }
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
     }
 
     @Override
@@ -83,12 +88,12 @@ public class Book {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Book book = (Book) o;
-        return id == book.id && page_number == book.page_number && Objects.equals(bookName, book.bookName) && Objects.equals(id_author, book.id_author) && Objects.equals(topic, book.topic) && Objects.equals(release_date, book.release_date) && Objects.equals(status, book.status);
+        return id == book.id && id_author == book.id_author && page_number == book.page_number && Objects.equals(bookName, book.bookName) && topic == book.topic && Objects.equals(release_date, book.release_date) && Objects.equals(status, book.status);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, bookName, id_author,page_number, topic, release_date, status);
+        return Objects.hash(id, bookName, id_author, page_number, topic, release_date, status);
     }
 
     @Override
