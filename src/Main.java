822bea79dd52cd54c06c332d6dbcfdd354797718
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2023-11-17 21:11:45 +0100

    Compile Java file using javac

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
-import Configuration.ConnectionDB;
-
-import java.sql.Connection;
+import static Service.ServiceAuthor.TestAuthor;
+import static Service.ServiceBook.TestCrudBook;
+import static Service.ServiceSubscribe.TestCrudSubscribe;
 
 public class Main {
     public static void main(String[] args) {
+        TestCrudBook();
+        TestAuthor();
+        TestCrudSubscribe();
     }
 }
